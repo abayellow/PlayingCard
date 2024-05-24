5550447e976b9e7bc418be170253d57541091b82
abayellow <abayellow@github.com>
2024-05-25 10:33:39 +1100

    Create folders and new PlayingCard View.

---
+++ PlayingCard/View/PlayingCardView.swift
@@ -0,0 +1,18 @@
+//
+//  PlayingCardView.swift
+//  PlayingCard
+//
+//  Created by Alexander Abanshin on 25.05.2024.
+//
+
+import UIKit
+
+class PlayingCardView: UIView {
+
+ 
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    
+
+}
