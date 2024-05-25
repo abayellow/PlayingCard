15a9505d0f1dadda4f681d147784e4d6ef1da4a3
abayellow <abayellow@github.com>
2024-05-25 11:08:25 +1100

    Creating FillCurcut

--- PlayingCard/View/PlayingCardView.swift
+++ PlayingCard/View/PlayingCardView.swift
@@ -11,18 +11,24 @@ class PlayingCardView: UIView {
 
  
     override func draw(_ rect: CGRect) {
-        if let contex = UIGraphicsGetCurrentContext() {
-            contex.addArc(center: CGPoint(x: bounds.midX, y: bounds.midY),
-                          radius: 100.0,
-                          startAngle: 0,
-                          endAngle: 2*CGFloat.pi,
-                          clockwise: true)
-            contex.setLineWidth(5.0)
-            UIColor.green.setFill()
-            UIColor.red.setStroke()
-            contex.strokePath()
-            contex.fillPath()
-        }
+//        if let contex = UIGraphicsGetCurrentContext() {
+//            contex.addArc(center: CGPoint(x: bounds.midX, y: bounds.midY),
+//                          radius: 100.0, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
+//            
+//            contex.setLineWidth(5.0)
+//            UIColor.green.setFill()
+//            UIColor.red.setStroke()
+//            contex.strokePath()
+//            contex.fillPath()
+//        }
+        let  path = UIBezierPath()
+        path.addArc(withCenter: CGPoint(x: bounds.midX, y: bounds.midY),
+                    radius: 100.0, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
+        path.lineWidth = 5.0
+        UIColor.green.setFill()
+        UIColor.red.setStroke()
+        path.stroke()
+        path.fill()
     }
     
 
